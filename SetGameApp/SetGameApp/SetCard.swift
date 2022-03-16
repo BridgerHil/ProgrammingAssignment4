a3b68d0cb3d1b3df6d8436b589bd3dbcd17387a8
BridgerHil <98505471+BridgerHil@users.noreply.github.com>
2022-03-16 15:52:14 -0600

    Add files via upload
    Set Game Updated 3/16

---
+++ SetGameApp/SetGameApp/SetCard.swift
@@ -0,0 +1,77 @@
+//
+//  SetCard.swift
+//  SetGameApp
+//
+//  Created by Bridger Hildreth on 3/6/22.
+//
+
+import Foundation
+import SwiftUI
+
+struct SetCard : Identifiable, Equatable {
+    
+    static func ==(lhs: SetCard, rhs: SetCard) -> Bool {
+        return (lhs.shape == rhs.shape &&
+                lhs.shade == rhs.shade &&
+                lhs.color == rhs.color &&
+                lhs.count == rhs.count &&
+                lhs.id == rhs.id)
+    }
+    
+    var shape: Shapes
+    var shade: Shades
+    var color: Colors
+    var count: Int
+    var id: Int
+    
+    var isSelected: Bool
+    var isMatched: Bool
+    var isMisMatched: Bool
+    
+    enum Shapes {
+        case diamond
+        case oval
+        case squiggle
+        
+        static var all = [Shapes.diamond, .oval, .squiggle]
+    }
+    
+    enum Shades {
+        case outlined
+        case striped
+        case filled
+        
+        static var all = [Shades.outlined, .striped, .filled]
+    }
+    
+    enum Colors {
+        case red
+        case green
+        case blue
+        
+        static var all = [Colors.red, .green, .blue]
+    }
+    
+    init(shape: Shapes, shade: Shades, color: Colors, count: Int, id: Int) {
+        self.shape = shape
+        self.shade = shade
+        self.color = color
+        self.count = count
+        self.id = id
+        self.isSelected = false
+        self.isMatched = false
+        self.isMisMatched = false
+    }
+    
+    init(shape: Shapes, shade: Shades, color: Colors, count: Int, id: Int, isSelected: Bool, isMatched: Bool, isMisMatched: Bool) {
+        self.shape = shape
+        self.shade = shade
+        self.color = color
+        self.count = count
+        self.id = id
+        self.isSelected = isSelected
+        self.isMatched = isMatched
+        self.isMisMatched = isMisMatched
+
+    }
+}
---
+++ SetGameApp/SetGameApp/SetGame.swift
@@ -0,0 +1,125 @@
+//
+//  SetGame.swift
+//  SetGameApp
+//
+//  Created by Bridger Hildreth on 3/6/22.
+//
+
+import Foundation
+
+struct SetGame {
+    
+    private(set) var currentDeck = SetCardDeck()
+    private(set) var cards = [SetCard]()
+    private(set) var discardDeck = [SetCard]()
+    private(set) var selectedCards = [SetCard]()
+    private(set) var indexArray = [Int]()
+    
+    mutating func selectCard(_ card: SetCard) {
+        
+        //Discards matched cards after selecting a 4th card
+        for i in (0...cards.count-1).reversed() {
+            print(i)
+            if cards[i].isMatched == true {
+                discardDeck.append(cards.remove(at: i))
+            }
+        }
+        
+        //Basic selection
+        if let chosenIndex = cards.firstIndex(where: {$0.id == card.id}) {
+            if cards[chosenIndex].isMatched {
+                return
+            }
+            cards[chosenIndex].isSelected.toggle()
+            if cards[chosenIndex].isSelected {
+                selectedCards.append(cards[chosenIndex])
+                indexArray.append(chosenIndex)
+            } else   {
+                for i in 0..<selectedCards.count-1 {
+                    if selectedCards[i] == cards[chosenIndex] {
+                        selectedCards.remove(at: i)
+                        indexArray.remove(at: i)
+                    }
+                }
+            }
+        }
+        
+        //Checks for a match
+        if selectedCards.count == 3 {
+            if setLogic() {
+                for i in 0..<indexArray.count {
+                    cards[indexArray[i]].isMatched = true
+                    cards[indexArray[i]].isSelected = false
+                }
+            } else {
+                for i in 0..<indexArray.count {
+                    cards[indexArray[i]].isMisMatched = true
+                    cards[indexArray[i]].isSelected = false
+                }
+            }
+            selectedCards.removeAll()
+            indexArray.removeAll()
+        }
+        
+    }
+    
+    //sets 12 cards
+    mutating func set12Cards() {
+        for _ in 0...11 {
+            cards.append(currentDeck.remove())
+        }
+    }
+    //makes sure to set cards on initization
+    init() {
+        set12Cards()
+    }
+    
+    //add 3 cards
+    mutating func add3Cards() {
+        if currentDeck.count() > 0 {
+            var flip = true
+            for i in 0...cards.count-1 {
+                if cards[i].isMatched {
+                    cards[i] = currentDeck.remove()
+                    flip = false
+                
+                }
+            }
+        
+            if flip {
+                for _ in 0...2 {
+                    cards.append(currentDeck.remove())
+                }
+            }
+        }
+    }
+    
+    //returns how many cards are left in deck
+    func deckCounter() -> Int {
+        return currentDeck.count()
+    }
+    
+    //starts new game
+    mutating func newGameButton() {
+        cards.removeAll()
+        currentDeck = SetCardDeck()
+        set12Cards()
+    }
+    
+    //The logic
+    func setLogic() -> Bool {
+        //These constants check in the first case if everything of that variable is the same
+        //The second case checks if everything is different from one another
+        //This is because in a set, cards can either be all the same or all different
+        let shapeLogic = ((selectedCards[0].shape == selectedCards[1].shape && selectedCards[1].shape == selectedCards[2].shape) || (selectedCards[0].shape != selectedCards[1].shape && selectedCards[0].shape != selectedCards[2].shape && selectedCards[1].shape != selectedCards[2].shape))
+        let colorLogic = ((selectedCards[0].color == selectedCards[1].color && selectedCards[1].color == selectedCards[2].color) || (selectedCards[0].color != selectedCards[1].color && selectedCards[0].color != selectedCards[2].color && selectedCards[1].color != selectedCards[2].color))
+        let shadeLogic = ((selectedCards[0].shade == selectedCards[1].shade && selectedCards[1].shade == selectedCards[2].shade) || (selectedCards[0].shade != selectedCards[1].shade && selectedCards[0].shade != selectedCards[2].shade && selectedCards[1].shade != selectedCards[2].shade))
+        let countLogic = ((selectedCards[0].count == selectedCards[1].count && selectedCards[1].count == selectedCards[2].count) || (selectedCards[0].count != selectedCards[1].count && selectedCards[0].count != selectedCards[2].count && selectedCards[1].count != selectedCards[2].count))
+        
+        if shapeLogic && colorLogic && shadeLogic && countLogic {
+            return true
+        } else {
+            return false
+        }
+    }    
+}
